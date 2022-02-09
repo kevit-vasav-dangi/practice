4cde2193ae74772e5c4584659c6ec3a19fb329b3
Vasav Kevit <vasav_dangi@kevit.io>
2022-02-09 18:02:42 +0530

    Revert "Adding business_logic.js"
    This reverts commit 0d520c4d47a0ef4b3c660ca4be2e2a6d293e8f27.

--- business_logic.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var app = express();
-
-app.get('/', function (req, res) {
-    console.log("Got a GET request for the homepage");
-    const userCount = Math.ceil(Math.random() * 100);
-    res.send(`Hello GET. Users are: ${userCount}`);
- })
